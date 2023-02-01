71e4185f2869be988ff712eab6ebe44d92d7c823
Dan Allen <dan@opendevise.com>
2023-01-31 18:11:38 -0700

    add hard line break after label on admonition paragraph

--- test/downdoc-test.js
+++ test/downdoc-test.js
@@ -950,15 +950,20 @@ describe('downdoc()', () => {
       const expected = heredoc`
       # Title
 
-      **📌 NOTE** Remember the oat milk.
+      **📌 NOTE**\\
+      Remember the oat milk.
 
-      **❗ IMPORTANT** Don’t forget the children!
+      **❗ IMPORTANT**\\
+      Don’t forget the children!
 
-      **💡 TIP** Look for the [warp](https://en.wikipedia.org/wiki/Warp_(video_games)) under the bridge.
+      **💡 TIP**\\
+      Look for the [warp](https://en.wikipedia.org/wiki/Warp_(video_games)) under the bridge.
 
-      **🔥 CAUTION** Slippery when wet.
+      **🔥 CAUTION**\\
+      Slippery when wet.
 
-      **⚠️ WARNING** The software you’re about to use has **not** been tested.
+      **⚠️ WARNING**\\
+      The software you’re about to use has **not** been tested.
       `
       expect(downdoc(input)).to.equal(expected)
     })
@@ -1412,7 +1417,8 @@ describe('downdoc()', () => {
       \`\`\`spoiler Always visible text
       This text won’t be visible until the user clicks the always visible text.
 
-      **💡 TIP** Click the **always visible text** to hide this text again.
+      **💡 TIP**\\
+      Click the **always visible text** to hide this text again.
       \`\`\`
       `
       expect(downdoc(input, { attributes: { 'markdown-collapsible-variant': 'spoiler' } })).to.equal(expected)
